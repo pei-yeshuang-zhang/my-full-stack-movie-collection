afc0913fd3a66b1e4a56513eea32952a3cbbc1a1
Pei Zhang <peip7@outlook.com>
2022-09-19 12:36:36 +1200

    map over home page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Home() {
+  const movies = useSelector((store) => store.movies)
+  console.log(movies)
+
   return (
     <>
       <h2>Home</h2>
+      {movies.map((movie) => {
+        const { title, imdb_id, watched, img } = movie
+        return (
+          <div key={movie.id}>
+            <img className="home-img" src={img} alt="movie" />
+            <p>{title}</p>
+          </div>
+        )
+      })}
     </>
   )
 }
