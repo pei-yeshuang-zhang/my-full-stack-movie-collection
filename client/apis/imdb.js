10a76c2f19ae136e3b36479fd926cccdb842aeb8
Pei Zhang <peip7@outlook.com>
2022-10-12 13:15:13 +1300

    finish imdb api call for details

--- client/apis/imdb.js
+++ client/apis/imdb.js
@@ -1,12 +1,21 @@
 import request from 'superagent'
 import { key } from '../../key'
 
-const imdbUrl = 'https://imdb-api.com/en/API/SearchMovie/'
+const imdbUrlSearch = 'https://imdb-api.com/en/API/SearchMovie/'
+const imdbUrlTitle = 'https://imdb-api.com/en/API/Title/'
 
 export function searchForMovie(movie) {
   // The param 'movie' is from AddMoive.movieSearch
-  return request.get(`${imdbUrl}${key}/${movie}`).then((res) => {
+  return request.get(`${imdbUrlSearch}${key}/${movie}`).then((res) => {
     console.log('IMDB SEARCH: ', res.body.results)
     return res.body.results
+  })
+}
+
+export function getMovieDetails(imdbID) {
+  // The param 'movie' is from AddMoive.movieSearch
+  return request.get(`${imdbUrlTitle}${key}/${imdbID}`).then((res) => {
+    console.log('IMDB TITLE: ', res.body)
+    return res.body
   })
 }
