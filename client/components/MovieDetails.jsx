6bf4ca4082d46c210b7ca56d2178afa0b1a6ac4d
Pei Zhang <peip7@outlook.com>
2022-10-14 17:40:26 +1300

    working on detail page

--- client/components/MovieDetails.jsx
+++ client/components/MovieDetails.jsx
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-// import { useSelector } from 'react-redux'
-// import { fetchMovieDetails } from '../apis/imdb'
-// import { getMovieDetails } from '../actions/movies'
+// import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { searchForMovie } from '../apis/imdb'
 
 function MovieDetails() {
-  const { imdb_id } = useParams()
+  // const { imdb_id } = useParams()
+  const oneMovie = useSelector((store) => store.movies)
+  console.log('ONE MOVIE: ', oneMovie)
 
-  const [movie, setMovie] = useState([])
+  // const [movie, setMovie] = useState([])
 
-  async function getMovieDetails(e) {
-    if (e && e.preventDefault) {
-      e.preventDefault()
-    }
-    const details = await fetchMovieDetails(imdb_id)
-    setMovie(details)
-  }
+  // async function getMovieDetails(e) {
+  //   if (e && e.preventDefault) {
+  //     e.preventDefault()
+  //   }
+  //   const details = await searchForMovie(imdb_id)
+  //   setMovie(details)
+  // }
 
-  useEffect(getMovieDetails(), [])
+  // useEffect(getMovieDetails(), [])
 
-  const { title, plot } = movie ? movie : {}
+  // const { Title } = movie ? movie : {}
 
   return (
     <>
-      <h2>{title}</h2>
-      <p>{plot}</p>
+      <h2>HELLO</h2>
     </>
   )
 }
