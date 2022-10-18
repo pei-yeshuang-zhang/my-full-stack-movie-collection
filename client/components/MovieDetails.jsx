f9da00653307b1694974736833a49e5bd31b191c
Pei Zhang <peip7@outlook.com>
2022-10-18 22:37:12 +1300

    watched button shows up on details page

--- client/components/MovieDetails.jsx
+++ client/components/MovieDetails.jsx
@@ -6,11 +6,24 @@ function MovieDetails() {
   const { imdb_id } = useParams()
   const movies = useSelector((store) => store.movies)
   const movie = movies.filter((movie) => movie.imdb_id === imdb_id)
+  // TODO: Finish onClick functions
+  function handleWatched() {}
+
+  function handleUnWatched() {}
 
   return (
     <>
       <img src={movie && movie[0].img} alt="movie"></img>
       <h2>{movie && movie[0].title}</h2>
+
+      <div>
+        {movie[0].watched == true ? (
+          <button onClick={handleWatched}>Wathched</button>
+        ) : (
+          <button onClick={handleUnWatched}>UnWathched</button>
+        )}
+      </div>
+
       <p>{movie && movie[0].plot}</p>
     </>
   )
