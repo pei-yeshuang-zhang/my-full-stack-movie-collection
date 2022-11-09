8114a096cfb6f531fa0c069d1619390113ed620b
Pei Zhang <peip7@outlook.com>
2022-11-09 15:55:20 +1300

    set up .env file

--- client/apis/imdb.js
+++ client/apis/imdb.js
@@ -1,11 +1,12 @@
 import request from 'superagent'
-import { key } from '../../key'
 
 const imdbUrlSearch = 'http://www.omdbapi.com/?apikey='
 
 export function searchForMovie(movie) {
   // The param 'movie' is from AddMoive.movieSearch
-  return request.get(`${imdbUrlSearch}${key}&t=${movie}`).then((res) => {
-    return res.body
-  })
+  return request
+    .get(`${imdbUrlSearch}${process.env.MOVIE_KEY}&t=${movie}`)
+    .then((res) => {
+      return res.body
+    })
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
+// Use these node modules to set up .env file
+const path = require('path')
+const dotenv = require('dotenv')
+
+// get the path of .env
+const envPath = path.join(__dirname, '..', '.env')
+
+// tell dotenv node module where to find our .env file
+dotenv.config({ path: envPath })
+
+//^ You must set up dotenv BEFORE you set up the server
+// so put the dotenv things right at the top of your index file
+
 const server = require('./server')
 
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-server.listen(PORT, () => {
+server.listen(port, function () {
   // eslint-disable-next-line no-console
-  console.log('Listening on port', PORT)
+  console.log('Listening on port', port)
+  console.log(process.env.MOVIE_KEY)
 })
