0e730649dcba5491591968323033841706715598
Pei Zhang <peip7@outlook.com>
2022-11-09 14:14:49 +1300

    working on App.test

---
+++ client/components/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import nock from 'nock'
+import { screen, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import App from './App'
+
+test.todo('The App component')
